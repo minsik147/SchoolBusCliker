f23c7fd2ce8d03e45351fad91a7de86767029189
Kim Jin <jinistory123@gmail.com>
2023-02-14 10:30:10 +0900

    date : 23.02.14 add : SchoolBusCliker Main Project File

---
+++ SchoolBusCliker/app/src/main/java/com/cafe24/as8794/schoolbuscliker/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cafe24.as8794.schoolbuscliker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
