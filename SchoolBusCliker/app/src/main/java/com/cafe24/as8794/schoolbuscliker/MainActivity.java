880c5493c4a8916bc3b7446ca5666e2c1fec8098
JiniStory123 <jinistory123@gmail.com>
2023-02-15 20:58:57 +0900

    date : 23-02-15 update : UI

--- SchoolBusCliker/app/src/main/java/com/cafe24/as8794/schoolbuscliker/MainActivity.java
+++ SchoolBusCliker/app/src/main/java/com/cafe24/as8794/schoolbuscliker/MainActivity.java
@@ -9,20 +9,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
-    Toolbar toolbar;
-    ActionBar actionBar;
+    Button bt_goSchool;
+    Button bt_goHome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 상단바 설정
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("예약하기");
+
+        // 아이디 값 설정
+        bt_goSchool = findViewById(R.id.bt_goSchool);
+        bt_goHome = findViewById(R.id.bt_goHome);
+
+        // 버튼 클릭 리스너
+        bt_goSchool.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                bt_goSchool.setBackgroundResource(R.drawable.selector_button2);
+                bt_goHome.setBackgroundResource(R.drawable.selector_button);
+            }
+        });
+
+        bt_goHome.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                bt_goSchool.setBackgroundResource(R.drawable.selector_button);
+                bt_goHome.setBackgroundResource(R.drawable.selector_button2);
+            }
+        });
     }
 
     // 뒤로가기
